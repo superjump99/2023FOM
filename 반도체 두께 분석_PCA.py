1ead4b1936006533a2ba8e4b0443b397a86498a9
Hansol Kim <khs_9902@naver.com>
2023-07-27 23:27:02 +0900

    fix inputdim 13

--- 반도체 두께 분석_PCA.py
+++ 반도체 두께 분석_PCA.py
@@ -22,7 +22,7 @@ test_X = test.iloc[:,1:]
 
 
 # PCA 진행
-pca = PCA(n_components=27)
+pca = PCA(n_components=13)
 pca.fit(train_X)
 
 train_score = pca.transform(train_X)
@@ -30,10 +30,10 @@ test_score = pca.transform(test_X)
 
 pd.DataFrame(pca.explained_variance_ratio_).plot(kind='bar', figsize=(20,5))
 
-for i in [10, 27, 50, 100, 120, 150, 226]:
-    print(i, " : ", pca.explained_variance_ratio_[:i].sum())
+# for i in [10, 27, 50, 100, 120, 150, 226]:
+#     print(i, " : ", pca.explained_variance_ratio_[:i].sum())
 
-PCA_NUM = [10, 27, 100, 226]
+PCA_NUM = [10, 13]
 history_save = []
 model_save = []
 
@@ -43,9 +43,8 @@ for i, n in enumerate(PCA_NUM):
     xTestData = test_score[:,:n]
     print(xData.shape)
 
-
 model = Sequential()
-model.add(Dense(units=1000, activation='relu', input_dim=27))
+model.add(Dense(units=1000, activation='relu', input_dim=13))
 model.add(Dropout(0.2))
 model.add(Dense(units=700, activation='relu'))
 model.add(Dropout(0.2))
@@ -53,12 +52,16 @@ model.add(Dense(units=400, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(units=200, activation='relu'))
 model.add(Dropout(0.2))
+model.add(Dense(units=100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=50, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(units=4, activation='linear'))
 
 model.compile(loss='mae', optimizer='adam', metrics=['mae'])
 
-epochs = 300
-batch_size = 4000
+epochs = 10
+batch_size = 500
 validation_split = 0.05
 history = model.fit(xData, train_Y, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
 
